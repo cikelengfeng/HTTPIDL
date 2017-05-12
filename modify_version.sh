4c93af541dbce9e7071eeffc1885d40aa0f8a001
DongXu <cikelengfeng@gmail.com>
2017-05-12 17:48:58 +0800

    [OPT] check modify version script argument

--- modify_version.sh
+++ modify_version.sh
@@ -1,7 +1,12 @@
-echo target version is $1
-sed -i.bak "s/s\.version.*=.*/s.version      = \"$1\"/g" HTTPIDL.podspec
-sed -i.bak "s/pod-.*-blue\.svg/pod-$1-blue\.svg/g" README.md
-git commit -am '[MOD] version'
-git tag $1
-git push origin --tags
-pod trunk push
+if echo $1 | grep -q -E '^\d+\.\d+\.\d+$'
+then
+    echo target version is $1
+    sed -i.bak "s/s\.version.*=.*/s.version      = \"$1\"/g" HTTPIDL.podspec
+    sed -i.bak "s/pod-.*-blue\.svg/pod-$1-blue\.svg/g" README.md
+    git commit -am '[MOD] version'
+    git tag $1
+    git push origin --tags
+    pod trunk push
+else
+    echo argument invalid, version must match '^\d+\.\d+\.\d+$' "(e.g. 1.2.42)"
+fi
