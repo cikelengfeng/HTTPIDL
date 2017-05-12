a1764899de9cda65358c41f389f014d2c9c72e35
DongXu <cikelengfeng@gmail.com>
2017-05-12 12:04:36 +0800

    [MOD] 放宽JSON格式要求

--- Sources/Runtime/HTTPIDLResponseDecoder.swift
+++ Sources/Runtime/HTTPIDLResponseDecoder.swift
@@ -7,13 +7,13 @@
 
 import Foundation
 
-private func decode(json: Any) throws -> ResponseContent {
+private func decode(json: Any) throws -> ResponseContent? {
     if let number = json as? NSNumber {
         return .number(value: number)
     }else if let tmp = json as? String {
         return .string(value: tmp)
     } else if let tmp = json as? [Any] {
-        return .array(value: try tmp.map({
+        return .array(value: try tmp.flatMap({
             return try decode(json: $0)
         }))
     } else if json is [String: Any], let tmp = json as? [String: Any] {
@@ -22,35 +22,32 @@ private func decode(json: Any) throws -> ResponseContent {
             ret[soGood.key] = try decode(json: soGood.value)
             return ret
         }))
-    }else {
-        throw HTTPResponseJSONDecoderError.unsupportedParameterType(value: json)
     }
+    return nil
 }
 
-private func decodeRoot(json: Any) throws -> ResponseContent {
+private func decodeRoot(json: Any) throws -> ResponseContent? {
     if let tmp = json as? [String: Any] {
         return .dictionary(value: try tmp.reduce([String: ResponseContent](), { (soFar, soGood) in
             var ret = soFar
             ret[soGood.key] = try decode(json: soGood.value)
             return ret
         }))
+    } else if let tmp = json as? [Any] {
+        return .array(value: try tmp.flatMap({ (element) in
+            return try decode(json: element)
+        }))
     } else {
-        throw HTTPResponseJSONDecoderError.illegalJSONObject(errorSource: json)
+        return try decode(json: json)
     }
 }
 
 public enum HTTPResponseJSONDecoderError: HIError {
-    case illegalJSONObject(errorSource: Any)
-    case unsupportedParameterType(value: Any)
     case JSONSerializationError(rawError: Error)
     
     public var errorDescription: String? {
         get {
             switch self {
-            case .illegalJSONObject(let errorSource):
-                return "response json decoder error: response不是字典类型，导致无法给response的属性赋值, error source: \(errorSource)"
-            case .unsupportedParameterType(let value):
-                return "response json decoder error: 不支持的参数类型 value: \(value)"
             case .JSONSerializationError(let rawError):
                 return (rawError as NSError).localizedDescription
             }
