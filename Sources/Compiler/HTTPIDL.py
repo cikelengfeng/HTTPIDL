483f7a2171d26289451fff26d45315e3234b516c
DongXu <cikelengfeng@gmail.com>
2017-09-20 13:28:37 +0800

    [MOD] version

--- Sources/Compiler/HTTPIDL.py
+++ Sources/Compiler/HTTPIDL.py
@@ -19,7 +19,7 @@ from docopt import docopt
 from Compiler import HTTPIDLCompiler
 
 if __name__ == '__main__':
-    arguments = docopt(__doc__, version='0.1.0')
+    arguments = docopt(__doc__, version='1.1.4')
     # print(arguments)
     output_directory_path = arguments['-o']
     compiler = HTTPIDLCompiler()
