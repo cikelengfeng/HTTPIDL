f801a09eb42a7826092745de0db91af2deb3bb07
DongXu <cikelengfeng@gmail.com>
2017-12-21 21:27:13 +0800

    [MOD] 修改版本之前必须先跑通单元测试

--- modify_version.sh
+++ modify_version.sh
@@ -1,14 +1,20 @@
 if echo $1 | grep -q -E '^\d+\.\d+\.\d+$'
 then
-    echo target version is $1
-    sed -i.bak "s/s\.version.*=.*/s.version      = \"$1\"/g" HTTPIDL.podspec
-    sed -i.bak "s/pod-.*-blue\.svg/pod-$1-blue\.svg/g" README.md
-    sed -i.bak "s/version='.*'/version='$1'/g" Sources/Compiler/HTTPIDL.py
-    git commit -am '[MOD] version'
-    git tag $1
-    git push origin
-    git push origin --tags
-    pod trunk push
+    echo unittest is running now
+    if xcodebuild test -workspace HTTPIDLDemo/HTTPIDLDemo.xcworkspace -scheme HTTPIDLDemoTests -destination 'platform=iOS Simulator,name=ip6 8.1,OS=8.1' -destination 'platform=iOS Simulator,name=iPhone 6,OS=11.1'
+    then 
+        echo target version is $1
+        sed -i.bak "s/s\.version.*=.*/s.version      = \"$1\"/g" HTTPIDL.podspec
+        sed -i.bak "s/pod-.*-blue\.svg/pod-$1-blue\.svg/g" README.md
+        sed -i.bak "s/version='.*'/version='$1'/g" Sources/Compiler/HTTPIDL.py
+        git commit -am '[MOD] version'
+        git tag $1
+        git push origin
+        git push origin --tags
+        pod trunk push
+    else 
+        echo unittest failed, go back and eat your shit
+    fi
 else
     echo argument invalid, version must match '^\d+\.\d+\.\d+$' "(e.g. 1.2.42)"
 fi
